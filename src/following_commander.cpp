b90a0ca4aca6f979ccd426aec409c3793703666b
fumiya <fumiyaonishi1016@gmail.com>
2020-12-08 13:30:58 +0900

    template matching

---
+++ src/following_commander.cpp
@@ -0,0 +1,67 @@
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+
+#include <geometry_msgs/Twist.h>
+
+#define I_T_MAX 0.785398 //PI/4
+#define V_T_MAX 3.1415
+
+geometry_msgs::Twist cmd_vel;
+
+float x = 0.5;
+float image_center = 0.5;
+float v = 0, theta = 0;
+float Kp_t=0.8, Ki_t=0, Kd_t=0;
+
+float PID(const float goal, const float present, const float Kp, const float Ki,
+          const float Kd, float &I, const float I_Limit, float (&diff)[2]);
+void saturate(float &value, const float limit);
+
+void msgCallback(const std_msgs::Float32::ConstPtr& msg){
+    x = msg->data;
+}
+
+int main(int argc, char **argv){
+    //config
+    ros::init(argc,argv,"following_commander");
+    ros::NodeHandle n;//make nodehandler
+
+    ros::Publisher pub=n.advertise<geometry_msgs::Twist>("cmd_vel",1);
+    ros::Subscriber sub=n.subscribe("BBox_center",1, msgCallback);
+
+    ros::Rate loop_rate(30);//looprate(Hz)
+    //main
+    while(ros::ok()){//keep moving while this node is activated 
+    //PID
+    static float I_t = 0, v_theta = 0.0;
+    static float diff_t[2] = {};
+    v_theta =
+        PID(x, image_center, Kp_t, Ki_t, Kd_t, I_t, I_T_MAX, diff_t);//for theta
+    saturate(v_theta, V_T_MAX);
+    cmd_vel.linear.x = 0.15;
+    cmd_vel.angular.z = v_theta;
+    //ROS_INFO("diff_d, %f %f",diff_d[0],diff_d[1]);
+    pub.publish(cmd_vel);  // publish
+
+    ros::spinOnce();//call CallBack function once
+    loop_rate.sleep();//sleep to maintain accurate loop rate
+    }
+    return 0;
+}
+
+float PID(const float goal, const float present, const float Kp, const float Ki, const float Kd, float &I, const float I_Limit, float (&diff)[2]){
+  diff[0] = diff[1];
+  diff[1] = present - goal;
+
+  float P = diff[1];
+  I += (diff[1] + diff[0]) / 2.0f / 30.0;
+  float D = (diff[1] - diff[0]) * 30.0;
+
+  saturate(I, I_Limit);
+  float v = Kp * P + Ki * I + Kd * D;
+  return v;
+}
+
+void saturate(float &value, const float limit){//prevent I value not to divergent
+  if (abs(value) > limit) value = limit;
+}
--- src/match_template.cpp
+++ src/match_template.cpp
@@ -1,59 +1,62 @@
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
 using namespace cv;
 
-class TemplateMatchingNode
-{
-private:
-    ros::NodeHandle nh_;    
-
+class TemplateMatchingNode {
+   private:
+    ros::NodeHandle nh_;
+    ros::Publisher pub;
     image_transport::ImageTransport it_;
     image_transport::Subscriber sub_;
-    
-    const char* template_img_path_ = "/home/kitajima/Pictures/template.png";
+
+    const char* template_img_path_ = "../Picture/coke_can.png";
     Mat template_img_;
     int match_method_ = CV_TM_SQDIFF;
+    std_msgs::Float32 pub_msg;
+    float BBox_center = 0;
+    float col = 800;
 
-public:
-    TemplateMatchingNode()
-    : it_(nh_)
-    {
-         sub_ = it_.subscribe("/camera/rgb/image_color", 1, &TemplateMatchingNode::camera_callback, this);
-         template_img_ = cv::imread(template_img_path_);
+   public:
+    TemplateMatchingNode() : it_(nh_) {
+        pub = nh_.advertise<std_msgs::Float32>("/BBox_center", 1);
+        sub_ = it_.subscribe("/camera/image_raw", 1,
+                             &TemplateMatchingNode::camera_callback, this);
+        template_img_ = cv::imread(template_img_path_);
+    }
+    void Publication(void) {
+        pub_msg.data = BBox_center/col;
+        pub.publish(pub_msg);
     }
 
-    ~TemplateMatchingNode()
-    {
-    }
+    ~TemplateMatchingNode() {}
 
-
-    void camera_callback(const sensor_msgs::ImageConstPtr& img_msg)
-    {
+    void camera_callback(const sensor_msgs::ImageConstPtr& img_msg) {
         Mat img;
-        try
-        {
+        try {
             img = cv_bridge::toCvShare(img_msg, "bgr8")->image;
             waitKey(30);
-        }
-        catch (cv_bridge::Exception &e)
-        {
-            ROS_ERROR("Could not convert from '%s' to 'bgr8'.", img_msg->encoding.c_str());
+        } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("Could not convert from '%s' to 'bgr8'.",
+                      img_msg->encoding.c_str());
         }
 
         // テンプレートマッチング
         // https://docs.opencv.org/3.4/de/da9/tutorial_template_matching.html
         Mat result;
-        int result_cols =  img.cols - template_img_.cols + 1;
+        col = img.cols;
+        int result_cols = img.cols - template_img_.cols + 1;
         int result_rows = img.rows - template_img_.rows + 1;
-        result.create( result_rows, result_cols, CV_32FC1 );
+        result.create(result_rows, result_cols, CV_32FC1);
 
         matchTemplate(img, template_img_, result, match_method_);
-        bool method_accepts_mask = (TM_SQDIFF == match_method_ || match_method_ == TM_CCORR_NORMED);
+        bool method_accepts_mask =
+            (TM_SQDIFF == match_method_ || match_method_ == TM_CCORR_NORMED);
         matchTemplate(img, template_img_, result, match_method_);
         normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
         double minVal;
@@ -63,20 +66,30 @@ public:
         Point matchLoc;
         minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
         matchLoc = minLoc;
-        rectangle(   img, matchLoc, Point(matchLoc.x + template_img_.cols , matchLoc.y + template_img_.rows ), Scalar::all(0), 2, 8, 0 );
-        rectangle(result, matchLoc, Point(matchLoc.x + template_img_.cols, matchLoc.y + template_img_.rows), Scalar::all(0), 2, 8, 0);
+        rectangle(img, matchLoc,
+                  Point(matchLoc.x + template_img_.cols,
+                        matchLoc.y + template_img_.rows),
+                  Scalar::all(0), 2, 8, 0);
+        rectangle(result, matchLoc,
+                  Point(matchLoc.x + template_img_.cols,
+                        matchLoc.y + template_img_.rows),
+                  Scalar::all(0), 2, 8, 0);
+        BBox_center = matchLoc.x + template_img_.cols / 2.0;
         imshow("subscribed img", img);
         imshow("result_window", result);
         waitKey(30);
     }
 };
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "matching_node");
-    TemplateMatchingNode node = TemplateMatchingNode(); 
-    ros::spin();
+    TemplateMatchingNode node = TemplateMatchingNode();
+    ros::Rate loop_rate(30);
+    while (ros::ok()) {
+        node.Publication();
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 
     return 0;
 }
